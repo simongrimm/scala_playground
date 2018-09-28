46465f91610887923c31029277aec1f82eb64c91
simongrimm <simongrimm@gmail.com>
2018-09-28 09:48:16 +0200

    refactoring ListProblems.scala

--- src/main/scala/ListProblems/ListProblems.scala
+++ src/main/scala/ListProblems/ListProblems.scala
@@ -1,65 +1,62 @@
 package ListProblems
 
+import scala.annotation.tailrec
+
 
 object ListProblems {
-
-  //P01
-  def last[A](list: List[A]): A = list match {
+  @tailrec
+  def last[A](as: List[A]): A = as match {
     case Nil => throw new NoSuchElementException
     case head::Nil => head
     case _::tail => last(tail)
   }
-
-  //P02
-  def lastButOne[A](list: List[A]): A = list match {
-    case Nil => throw new NoSuchElementException
-    case _::Nil  => throw new NoSuchElementException
+  @tailrec
+  def lastButOne[A](as: List[A]): A = as match {
+    case Nil | _::Nil => throw new NoSuchElementException
     case last_but_one::_::Nil => last_but_one
     case _::tail => lastButOne(tail)
   }
-
-  //P03
-  def kthElement[A](k: Long, list: List[A]): A = (k, list) match {
+  @tailrec
+  def kthElement[A](k: Long, as: List[A]): A = (k, as) match {
     case (_, Nil) => throw new NoSuchElementException
     case (0, head::_) => head
     case (m, _ ::tail) => kthElement(m - 1, tail)
 
   }
-
-  //P04
-  def length[A](list: List[A]): Long = list match {
-    case Nil => 0
-    case _::Nil => 1
-    case _::tail => 1 + length(tail)
+  @tailrec
+  def length[A](as: List[A], acc: Long = 0): Long = as match {
+    case Nil => 0 + acc
+    case _::tail => length(tail, acc+1)
   }
 
-  //P05
-  def reverse[A](list: List[A]): List[A] = list match {
+  def reverse[A](as: List[A]): List[A] = as match {
     case Nil => Nil
     case head::last::Nil => last::head::Nil
     case head::tail => reverse(tail):::head::Nil
   }
 
 
-
-
-
-
-
-
-  type OrderedView[A] = A => Ordered[A]
-
-  def isSorted[A: OrderedView](list: List[A]): Boolean = list.sliding(2).forall(s => s(0) <= s(1))
-
-  def insert[A: OrderedView](new_element: A, list: List[A]): List[A] = list match {
-    case Nil => List(new_element)
-    case head :: tail => assert(head <= tail.head); if (new_element <= head) new_element :: list else head :: insert(new_element, tail)
+  def isSorted[A](as: List[A])(implicit ordering: Ordering[A]): Boolean = {
+    import ordering._
+    as.sliding(2).forall(t => t(0) <= t(1))
   }
 
-  def sort[A: OrderedView](list: List[A]): List[A] = list match {
-    case Nil => Nil
-    case head :: tail => insert(head, sort(tail))
+  def insertIntoSorted[A](a: A, as: List[A])(implicit ordering: Ordering[A]): List[A] = {
+    import ordering._
+    as match {
+      case Nil => List(a)
+      case head :: tail =>
+        assert(tail.isEmpty || head <= tail.head)
+        if (a <= head) a :: as
+        else head :: insertIntoSorted(a, tail)
+    }
   }
+
+  def insertionSort[A](as: List[A])(implicit ordering: Ordering[A]): List[A] = as match {
+      case Nil => Nil
+      case head :: tail => insertIntoSorted(head, insertionSort(tail))
+  }
+
 
 
 
@@ -73,11 +70,13 @@ object ListProblems {
 
   def main(args: Array[String]) {
     val l = List('b', 'z', 'D')
-    val l2 = insert[Char]('b', l)
-    //val l = List(5, 6, 6, 7, 23)
-    //val l2 = insert(24, l)
-    for (i <- l2)
-      print(i + " ")
+    println(s"$l is sorted?: ${isSorted(l)}")
+    val l2 = insertIntoSorted[Char]('b', l)
+    println(s"$l2 is sorted?: ${isSorted(l2)}")
+    val l3 = List(8, 5, 6, 6, 7, 23)
+    println(s"$l3 is sorted?: ${isSorted(l3)}")
+    val l4 = insertionSort(l3)
+    println(s"$l4 is sorted?: ${isSorted(l4)}")
     println()
     println(s"last:${last(l2)}")
     println(s"last_but_one:${lastButOne(l2)}")
