c469168b486145cec466d9b6f84f64a61e4ce51a
simongrimm <42965461+simongrimm@users.noreply.github.com>
2018-10-02 07:40:21 +0000

    Update DuplicateEncoder.scala

--- src/main/scala/katas/DuplicateEncoder.scala
+++ src/main/scala/katas/DuplicateEncoder.scala
@@ -1,5 +1,5 @@
 /*
-https://ide.geeksforgeeks.org/ptwYHeKuWg
+https://ide.geeksforgeeks.org/rcfxJaZnkh
 The goal of this exercise is to convert a string to a new string where each character 
 in the new string is '(' if that character appears only once in the original string, 
 or ')' if that character appears more than once in the original string. 
