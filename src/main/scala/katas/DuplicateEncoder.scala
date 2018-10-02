64595e13f1267d204d49cb7f9871261d4508062a
simongrimm <42965461+simongrimm@users.noreply.github.com>
2018-10-02 07:38:09 +0000

    Update DuplicateEncoder.scala

--- src/main/scala/katas/DuplicateEncoder.scala
+++ src/main/scala/katas/DuplicateEncoder.scala
@@ -1,13 +1,13 @@
 /*
 https://ide.geeksforgeeks.org/ptwYHeKuWg
-The goal of this exercise is to convert a string to a new string where each character in the new string is '(' if that character appears only once in the original string, or ')' if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.Examples:
+The goal of this exercise is to convert a string to a new string where each character 
+in the new string is '(' if that character appears only once in the original string, 
+or ')' if that character appears more than once in the original string. 
+Ignore capitalization when determining if a character is a duplicate.Examples:
 
 "din" => "((("
-
 "recede" => "()()()"
-
 "Success" => ")())())"
-
 "(( @" => "))(("
 */
 
@@ -23,8 +23,6 @@ object Solution {
     println(duplicateEncode("din"))
     println(duplicateEncode("recede"))
     println(duplicateEncode("Success"))
+    println(duplicateEncode("(( @"))
   }
-}
-
-
 }
