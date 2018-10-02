1bb88e8ea2e03b6808dee62392e057c72a1acefb
simongrimm <42965461+simongrimm@users.noreply.github.com>
2018-10-02 07:40:42 +0000

    Update DuplicateEncoder.scala

--- src/main/scala/katas/DuplicateEncoder.scala
+++ src/main/scala/katas/DuplicateEncoder.scala
@@ -13,12 +13,10 @@ Ignore capitalization when determining if a character is a duplicate.Examples:
 
 object Solution {
  
- 
   def duplicateEncode(word: String): String = {
     word.map(char => word.count(c => char.toLower == c.toLower)).map(wc => { if (wc > 1) ')' else '(' }).mkString
   }
 
- 
   def main(args: Array[String]) {
     println(duplicateEncode("din"))
     println(duplicateEncode("recede"))
